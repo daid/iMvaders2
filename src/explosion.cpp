b3986f7294f685cab5ec9a78764a68ddbc6c2c7b
Daid <daid303@gmail.com>
2019-10-28 21:41:54 +0000

    Compile fix

--- src/explosion.cpp
+++ src/explosion.cpp
@@ -16,11 +16,11 @@ Explosion::Explosion(sp::Vector2d position, float size, sp::Vector2d velocity)
     {
         parameters.position = sp::Vector3f(0, 0, 0);
         parameters.velocity = (sp::Vector3f(sp::random(-1, 1), sp::random(-1, 1), 0.0)) * sp::random(1.5 * size, 2.5 * size) + sp::Vector3f(velocity.x * 0.7, velocity.y * 0.7, 0).normalized();
-        parameters.acceleration = -parameters.velocity * 0.5f;
-        parameters.start_size = size * 0.7;
-        parameters.end_size = size * 0.3;
-        parameters.start_color = sp::Color(1, 0.5, 0.2);
-        parameters.end_color = sp::Color(0, 0, 0);
+        //parameters.acceleration = -parameters.velocity * 0.5f;
+        //parameters.start_size = size * 0.7;
+        //parameters.end_size = size * 0.3;
+        //parameters.start_color = sp::Color(1, 0.5, 0.2);
+        //parameters.end_color = sp::Color(0, 0, 0);
         parameters.lifetime = 0.25;
         emit(parameters);
     }
@@ -30,11 +30,11 @@ Explosion::Explosion(sp::Vector2d position, float size, sp::Vector2d velocity)
     {
         parameters.position = sp::Vector3f(sp::random(-1, 1), sp::random(-1, 1), 0.0).normalized() * sp::random(0.3 * size, 0.5 * size);
         parameters.velocity = parameters.position * 7.0f + sp::Vector3f(velocity.x, velocity.y, 0);
-        parameters.acceleration = -parameters.velocity;
-        parameters.start_size = size * 0.3;
-        parameters.end_size = 0.0;
-        parameters.start_color = sp::Color(1, 0.2, 0.1);
-        parameters.end_color = sp::Color(0, 0, 0);
+        //parameters.acceleration = -parameters.velocity;
+        //parameters.start_size = size * 0.3;
+        //parameters.end_size = 0.0;
+        //parameters.start_color = sp::Color(1, 0.2, 0.1);
+        //parameters.end_color = sp::Color(0, 0, 0);
         parameters.lifetime = 0.4;
         emit(parameters);
     }
